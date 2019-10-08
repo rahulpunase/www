53093ce2697874b3f0af48775d1ceda64d24002f
Rahul Punase <rahulpunase@commons-MacBook-Air.local>
2019-10-08 21:05:20 +0530

    Added transaction for adding data to user login

--- playcart-server/server/consts/query.consts.js
+++ playcart-server/server/consts/query.consts.js
@@ -1,3 +1,4 @@
 const db = "`playcart`.";
 export const SELECT_COUNTRY_CODE = "SELECT `id`, `name`, `phonecode` from " + db + "`country_with_code`";
 export const INSERT_INTO_PARTY = "INSERT INTO " + db + "`party`(`party_type_id`,`description`,`status_id`,`created_stamp`,`rowstate`) VALUES (?, ?, ?, now(), 1)";
+export const INSERT_INTO_USER_LOGIN = "INSERT INTO " + db + "`user_login`(`PARTY_ID`,`PHONE_NUMBER`,`EMAIL`,`CURRENT_PASSWORD`,`IS_SYSTEM`,`ENABLED`,`REQUIRE_PASSWORD_CHANGE`,`SUCCESSIVE_FAILED_LOGINS`,`LAST_UPDATED_STAMP`,`CREATED_STAMP`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, now(), now())";
--- playcart-server/server/controllers/authentication.controller.js
+++ playcart-server/server/controllers/authentication.controller.js
@@ -1,16 +1,21 @@
 import pool from '../db/connection.db';
 import { PARTY_TYPE_ID } from '../consts/foreign.consts';
-import { INSERT_INTO_PARTY } from '../consts/query.consts';
+import { INSERT_INTO_PARTY, INSERT_INTO_USER_LOGIN } from '../consts/query.consts';
 import uniqid from 'uniqid';
 
 export const createUserAccount = async (req, res, next) => {
     let connection = null;
+    const body = req.body;
+    // start with the empty check ---- >
     try {
         connection = await pool.getConnection();
         await connection.query("START TRANSACTION");
         const sql  = await connection.format(INSERT_INTO_PARTY, 
             [PARTY_TYPE_ID.PERSON, '', '']);
         const [fields] = await connection.query(sql);
+        const insertId = fields.insertId;
+        // INSERT_INTO_USER_LOGIN
+        await connection.query(INSERT_INTO_USER_LOGIN, [insertId]);
         await connection.query("COMMIT");
         res.json(fields);
     } catch (e) {
